63d0cb97d33d828eb19c9b0a91af7be1032a5cba
Simon Coulombe <simoncoulombe@gmail.com>
2022-01-21 13:07:37 -0800

    get data from CSV, not SQL

--- R/app_server.R
+++ R/app_server.R
@@ -26,24 +26,36 @@ app_server <- function(input, output, session) {
     library(gwellsshiny)
   })
   # get the data from CSV (or SQL if this ever get implemented)
-  waiter_show(html = spin_fading_circles())  
-  BCGOV_DB <- Sys.getenv("BCGOV_DB")
-  BCGOV_HOST <- Sys.getenv("BCGOV_HOST")
-  BCGOV_USR <- Sys.getenv("BCGOV_USR")
-  BCGOV_PWD <- Sys.getenv("BCGOV_PWD")
-  if (is.null(BCGOV_DB) || is.null(BCGOV_HOST)|| is.null(BCGOV_USR) || is.null(BCGOV_PWD)) stop("go away -- set up sql access as environment variables BCGOV_DB, BCGOV_HOST, BCGOV_USR, BCGOV_PWD")
+  waiter_show(html = spin_fading_circles()) 
   
-  con1 <- DBI::dbConnect(
-    #RPostgreSQL::PostgreSQL(),
-    RPostgres::Postgres(),
-    dbname = Sys.getenv("BCGOV_DB"),
-    host = Sys.getenv("BCGOV_HOST"),
-    user = Sys.getenv("BCGOV_USR"),
-    password=Sys.getenv("BCGOV_PWD")
-  )
+  data_source <- "csv"
   
-  # get data from the 3 tables, left join it, create some flags etc..
-  prepared_gwells <- prepare_all_data(con1)
+  if(data_source == "sql"){ #are we getting the data from SQL?
+    BCGOV_DB <- Sys.getenv("BCGOV_DB")
+    BCGOV_HOST <- Sys.getenv("BCGOV_HOST")
+    BCGOV_USR <- Sys.getenv("BCGOV_USR")
+    BCGOV_PWD <- Sys.getenv("BCGOV_PWD")
+    if (is.null(BCGOV_DB) || is.null(BCGOV_HOST)|| is.null(BCGOV_USR) || is.null(BCGOV_PWD)) stop("go away -- set up sql access as environment variables BCGOV_DB, BCGOV_HOST, BCGOV_USR, BCGOV_PWD")
+    
+    con1 <- DBI::dbConnect(
+      #RPostgreSQL::PostgreSQL(),
+      RPostgres::Postgres(),
+      dbname = Sys.getenv("BCGOV_DB"),
+      host = Sys.getenv("BCGOV_HOST"),
+      user = Sys.getenv("BCGOV_USR"),
+      password=Sys.getenv("BCGOV_PWD")
+    )
+    
+    # get data from the 3 tables, left join it, create some flags etc..
+    full_data <- download_data_sql(con1)
+  }
+  
+  if(data_source == "csv"){
+    full_data <- download_data_csv()
+    
+  }
+  
+  prepared_gwells <- prepare_all_data(full_data)
   
   #ok we are ready to roll!  
   message("unhiding waiter")
--- R/fct_server_prepare_data.R
+++ R/fct_server_prepare_data.R
@@ -1,4 +1,4 @@
-prepare_all_data <- function(connection){
+download_data_sql <- function(connection){
   message("preparing data")
   message("download wells")
   wells <- dbGetQuery(connection,
@@ -34,7 +34,46 @@ prepare_all_data <- function(connection){
   gc()
   
   message("available memory:", getAvailMem())
+  return(full_data)
+}
+
+download_data_csv <- function(){
+  message("download wells csv ")
+  wells <- readr::read_csv("https://github.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/raw/main/data/wells.csv")
   
+  my_wtn <- wells$well_tag_number
+  message("download geocoded csv ")
+  geocode <- readr::read_csv("https://github.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/raw/main/data/wells_geocoded.csv")
+  
+  
+  message("download qa csv")
+  qa <- readr::read_csv("https://github.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/raw/main/data/wells_qa.csv")
+  
+  colnames_in_more_than_one_db <- c("identification_plate_number", "well_identification_plate_attached", "well_status_code", "well_class_code", "well_subclass", "licenced_status_code", "intended_water_use_code", "observation_well_number", "obs_well_status_code", "water_supply_system_name", "water_supply_system_well_name", "street_address", "city", "legal_lot", "legal_plan", "legal_district_lot", "legal_block", "legal_section", "legal_township", "legal_range", "land_district_code", "legal_pid", "well_location_description", "latitude_decdeg", "longitude_decdeg", "utm_zone_code", "utm_northing", "utm_easting", "coordinate_acquisition_code", "construction_start_date", "construction_end_date", "alteration_start_date", "alteration_end_date", "decommission_start_date", "decommission_end_date", "driller_name", "consultant_name", "consultant_company", "diameter_inches", "total_depth_drilled_ft_bgl", "finished_well_depth_ft_bgl", "final_casing_stick_up_inches", "bedrock_depth_ft_bgl", "ground_elevation_ft_asl", "ground_elevation_method_code", "static_water_level_ft_btoc", "well_yield_usgpm", "well_yield_unit_code", "artesian_flow_usgpm", "artesian_pressure_psi", "well_cap_type", "well_disinfected_code", "well_orientation_code", "alternative_specs_submitted", "surface_seal_material_code", "surface_seal_method_code", "surface_seal_depth_ft", "backfill_type", "backfill_depth_ft", "liner_material_code", "liner_diameter_inches", "liner_thickness_inches", "surface_seal_thickness_inches", "liner_from_ft_bgl", "liner_to_ft_bgl", "screen_intake_method_code", "screen_type_code", "screen_material_code", "other_screen_material", "screen_information", "screen_opening_code", "screen_bottom_code", "other_screen_bottom", "filter_pack_from_ft", "filter_pack_to_ft", "filter_pack_material_code", "filter_pack_thickness_inches", "filter_pack_material_size_code", "development_hours", "development_notes", "water_quality_colour", "water_quality_odour", "yield_estimation_method_code", "yield_estimation_rate_usgpm", "yield_estimation_duration_hours", "static_level_before_test_ft_btoc", "drawdown_ft_btoc", "hydro_fracturing_performed", "hydro_fracturing_yield_increase", "decommission_reason", "decommission_method_code", "decommission_details", "decommission_sealant_material", "decommission_backfill_material", "comments", "ems", "person_responsible", "company_of_person_responsible", "aquifer_id", "avi_years", "storativity", "transmissivity_m_2_s", "hydraulic_conductivity_m_s", "specific_storage_1_m", "specific_yield", "testing_method", "testing_duration_hours", "analytic_solution_type", "boundary_effect_code", "aquifer_lithology_code", "artesian_pressure_head_ft_agl", "artesian_conditions", "full_address", "civic_number", "civic_number_suffix", "street_name", "street_type", "is_street_type_prefix", "street_direction", "is_street_direction_prefix", "street_qualifier", "locality_name")
+  
+  message("left joining")
+  full_data <- wells %>% 
+    dplyr::left_join(geocode) %>% 
+    dplyr::left_join(qa %>% dplyr::select(-dplyr::all_of(colnames_in_more_than_one_db))) # %>%
+  
+  message("done joining")
+  
+  # the shinyapps.io free plan  is short  on memory, let's help us to stay within limits by erasing tables and running garbade control
+  rm(wells)
+  rm(geocode)
+  rm(qa)
+  gc()
+  
+  message("available memory:", getAvailMem())  
+  
+  return(full_data)  
+}
+
+
+
+
+
+prepare_all_data <- function(full_data){
   z <- full_data %>%
     dplyr::mutate(
       cross_referenced = tidyr::replace_na(stringr::str_detect(toupper(comments), "X REF|XREF|X-REF|CROSS REF|CROSSREF|CROSS-REF"), FALSE),
@@ -96,13 +135,13 @@ prepare_all_data <- function(connection){
       table3_flag =  table3_missing_lat_long_flag + table3_missing_finished_well_depth_flag,
       my_intended_water_use =
         forcats::fct_explicit_na(
-        factor(dplyr::case_when(
-          well_class_code == "WATR_SPPLY" &  intended_water_use_code == "UNK"~ "UNK_USE",
-          well_class_code == "WATR_SPPLY" ~ intended_water_use_code,
-          TRUE ~  "Non-Water Supply"), 
-               levels = c("DOM", "UNK_USE","DWS","COM","IRR","OTHER","TST","OBS","OP_LP_GEO", "Non-Water Supply")
-        ),
-        na_level = "MISS_USE"
+          factor(dplyr::case_when(
+            well_class_code == "WATR_SPPLY" &  intended_water_use_code == "UNK"~ "UNK_USE",
+            well_class_code == "WATR_SPPLY" ~ intended_water_use_code,
+            TRUE ~  "Non-Water Supply"), 
+            levels = c("DOM", "UNK_USE","DWS","COM","IRR","OTHER","TST","OBS","OP_LP_GEO", "Non-Water Supply")
+          ),
+          na_level = "MISS_USE"
         )
     )%>%
     dplyr::mutate(
